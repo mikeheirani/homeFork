ad28ee9a3d0aa33569588291991b2e3df430c112
Mike <mike.heirani@gmail.com>
2020-06-05 21:37:16 +0100

    Include related links and IP based list displaying

--- script/script.js
+++ script/script.js
@@ -1,36 +1,40 @@
 const Config = {
-    name: "user",
+    name: "Mike",
     scale: 1,
     Links: [
         [
-            "site",
+            "Work",
             [
-                ["link", "https://www.example.com"],
-                ["link", "https://www.example.com"]
+                ["AWS", "https://d-9c673dcd3b.awsapps.com/start#/"],
+                ["Jira", "https://jira.smartstream-stp.com/"],
+                ["Confluence", "https://confluence.smartstream-stp.com/"]
             ]
         ],
         [
-            "site",
+            "External",
             [
-                ["link", "https://www.example.com"],
-                ["link", "https://www.example.com"]
+                ["Bookstack", "http://mikeheirani.co.uk:7001"],
+                ["GitHub", "https://github.com/mikeheirani?tab=repositories"],
+                ["Plex", "https://app.plex.tv/desktop#"],
+                ["Torrentleech", "https://www.torrentleech.org"]
             ]
         ],
         [
-            "site",
+            "Internal",
             [
-                ["link", "https://www.example.com"],
-                ["link", "https://www.example.com"],
-                ["link", "https://www.example.com"]
+                ["Unraid", "http://192.168.0.36:1234/login"],
+                ["Transmission", "http://192.168.0.36:9091/transmission/web/"],
+                ["BookStack", "http://192.168.0.36/login"],
+                ["Jackett", "http://192.168.0.36:9117"],
+                ["Sonarr", "http://192.168.0.36:8989"],
+                ["Lychee", "http://192.168.0.36:8060"]
             ]
         ],
         [
-            "site",
+            "Social",
             [
-                ["link", "https://www.example.com"],
-                ["link", "https://www.example.com"],
-                ["link", "https://www.example.com"],
-                ["link", "https://www.example.com"]
+                ["Reddit", "https://reddit.com"],
+                ["Facebook", "https://facebook.com"]
             ]
         ]
     ]
@@ -44,20 +48,45 @@ const Main = (() => {
 
     const init = () => {
         list.innerHTML = Config.Links.map(([gName, Links]) => `
-            <li>
+            <li id="${gName}">
                 <h1 onclick="this.parentNode.classList.toggle('hideChildren')">${gName}</h1>
                 <ul>
                     ${Links.map(([lName, url]) => `
                         <li>
-                            <a href="${url}">${lName}</a>
+                            <a target="_blank" href="${url}">${lName}</a>
                         </li>`
                     ).join("")}
                 </ul>
-            </li>` 
+            </li>`
+
         ).join("")
-        
+
         names.forEach(el => {
             el.innerText = Config.name;
+        });
+
+        document.getElementById("Internal").className = "hideChildren";
+        document.getElementById("Work").className = "hideChildren";
+
+        var homeIPAddresses = ["82.37.122.208"];
+        var workIPAddresses = ["64.94.171.243"];
+
+        $.getJSON('http://gd.geobytes.com/GetCityDetails?callback=?', function(data) {
+
+            const obj = JSON.parse(JSON.stringify(data, null, 2));
+            console.log(obj.geobytesipaddress);
+            console.log(obj);
+
+            if(workIPAddresses.includes(obj.geobytesipaddress))
+            {
+                document.getElementById("Work").className = "";
+            }
+
+            if(homeIPAddresses.includes(obj.geobytesipaddress))
+            {
+                document.getElementById("Internal").className = "";
+            }
+
         });
 
         document.addEventListener("keydown", e => e.key.length === 1 && search.focus());
